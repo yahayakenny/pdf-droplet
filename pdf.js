aee8084fbdb2e353218c25c3549b6e954fda67f1
yahayakenny <y.hkehinde@yahoo.com>
2022-07-09 20:09:00 +0100

    added server port

--- pdf.js
+++ pdf.js
@@ -108,3 +108,18 @@ let docDefination = {
 let pdfDoc = pdfmake.createPdfKitDocument(docDefination, {});
 pdfDoc.pipe(fs.createWriteStream("pdfs/test.pdf"));
 pdfDoc.end();
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 5000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World, we are testing pdf generation on Node');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
